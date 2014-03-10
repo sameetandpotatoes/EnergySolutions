0aca79886aafb6fb07d7a8bb97e092a17dbaa1ba
Sameet Sapra <sameetandpotatoes@gmail.com>
2014-03-10 18:00:20 -0500

    trying sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,16 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.pure-energy.org"
 SitemapGenerator::Sitemap.create do
-  add biomass_path, :priority => 0.7, :changefreq => 'daily'
-  add wind_path, :priority => 0.7, :changefreq => 'daily'
-  add solar_path, :priority => 0.7, :changefreq => 'daily'
-  add geothermal_path, :priority => 0.7, :changefreq => 'daily'
-  add hydroelectric_path, :priority => 0.7, :changefreq => 'daily'
-  add home_path, :priority => 0.7, :changefreq => 'daily'
-  add biofuel_path, :priority => 0.7, :changefreq => 'daily'
-  add sources_path, :priority => 0.7, :changefreq => 'daily'
-  add maps_path, :priority => 0.7, :changefreq => 'daily'
-  add results_path, :priority => 0.7, :changefreq => 'daily'
-  add all_path, :priority => 0.7, :changefreq => 'daily'
+  add biomass_path, :priority => 0.9, :changefreq => 'daily'
+  add wind_path, :priority => 0.9, :changefreq => 'daily'
+  add solar_path, :priority => 0.9, :changefreq => 'daily'
+  add geothermal_path, :priority => 0.9, :changefreq => 'daily'
+  add hydroelectric_path, :priority => 0.9, :changefreq => 'daily'
+  add home_path, :priority => 0.9, :changefreq => 'daily'
+  add biofuel_path, :priority => 0.9, :changefreq => 'daily'
+  add sources_path, :priority => 0.9, :changefreq => 'daily'
+  add maps_path, :priority => 0.9, :changefreq => 'daily'
+  add results_path, :priority => 0.9, :changefreq => 'daily'
+  add all_path, :priority => 0.9, :changefreq => 'daily'
 end
+SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
